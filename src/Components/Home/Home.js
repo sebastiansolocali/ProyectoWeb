718cd2018f4b85e64432171f3a69767ffa998852
juanda3098 <juandapore3098@gmail.com>
2019-11-04 11:12:29 -0500

    Se completó la vista Home

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 
 import './Home.css'
 
+import fondo from '../../Img/fondo_home.png'
+
 class Home extends Component {
 
     constructor(props) {
@@ -13,22 +15,36 @@ class Home extends Component {
 
     render() {
         return (
-            <section className="o-section-home">
-                <div className="o-home-container">
+            <div className="o-home">
+                <img alt="" src={fondo} className="o-img-fondo"/>
+                <section className="o-home-section">
                     <div className="o-header">
                         <h5>Hypnotic</h5>
                         <div className="o-access-container">
-                            <button className="o-button-signin">Sign in</button>
-                            <button>Login</button>
+                            <button className="o-button o-button-signin">Sign in</button>
+                            <button className="o-button o-button-login">Login</button>
                         </div>
                     </div>
                     <div className="o-title-home">
-                        <h1>E-come</h1>
-                        <h4>Welcome</h4>
+                        <h1 className="o-text">E-come</h1>
+                        <h4 className="o-text">Welcome</h4>
                     </div>
-                    <button className="o-button-readmore">Read More</button>
-                </div>
-            </section>
+                    <button className="o-button o-button-readmore">Read More</button>
+                </section>
+                <section className="o-readmore-section">
+                    <div className="o-readmore-text">
+                        <h5>Desconecta tus electrodomésticos desde un solo lugar</h5>
+                    </div>
+                    <div className="o-readmore-text">
+                        <h5>¿Se te olvidó apagar el televisor? Tranquilo, nosotros te lo apagamos</h5>
+                    </div>
+                </section>
+                <section className="o-info-section">
+                    <div className="o-info">
+                        <h5>Controla el consumo energético de tus electrodomésticos, desconéctalos y ayuda al medio ambiente</h5>
+                    </div>
+                </section>
+            </div>
         )
     }
 
