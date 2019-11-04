eda13bc743430e685948acc949d28d1581dd733e
juanda3098 <juandapore3098@gmail.com>
2019-11-04 17:17:48 -0500

    Se adelantó la vista de Login

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,5 +1,9 @@
 import React, {Component} from 'react'
 
+import Header1 from '../Header/Header1'
+
+import fondo from '../../Img/fondo_login.png'
+
 import './Login.css'
 
 class Login extends Component {
@@ -13,7 +17,18 @@ class Login extends Component {
 
     render(){
         return(
-            <div>Login</div>
+            <div className="o-login">
+                <img alt="" src={fondo} className="o-img-fondo-login"/>
+                <section className="o-login-section">
+                    <Header1 className="o-header"/>
+                    <div className="o-box-login">
+                        <h2>E-come</h2>
+                        <div className="o-info-login">
+                            
+                        </div>
+                    </div>
+                </section>
+            </div>
         )
     }
 
