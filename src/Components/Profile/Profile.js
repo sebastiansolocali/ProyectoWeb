36b717c4170105656b793fbb9c2d1545422c82ba
cristianreyes214 <cristian.reyes@uao.edu.co>
2019-11-06 14:55:12 -0500

    Se creó el componente Header2

---
+++ src/Components/Profile/Profile.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react'
+
+import Header from '../Header/Header2'
+
+import './Profile.css'
+
+class Profile extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return(
+            <div>
+                <Header />
+            </div>
+        )
+    }
+
+}
+
+export default Profile
