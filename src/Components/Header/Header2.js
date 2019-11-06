b3e5b666d6f73f5393d6ccc43ba9e025bd8a1383
cristianreyes214 <cristian.reyes@uao.edu.co>
2019-11-06 14:55:40 -0500

    Se creó el componente Header2

---
+++ src/Components/Header/Header2.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './Header2.css'
+
+const Header2 = () => {
+    return (
+        <div className="o-header2">
+            <div className="o-access-container">
+                <h2>E-come</h2>
+                <h4>by Hypnotic</h4>
+            </div>
+            <img alt=""  className="o-img-profile" />
+        </div>
+    )
+}
+
+export default Header2
