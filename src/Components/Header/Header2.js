12baa61396659dbeb186d7376ac9556cd5e18abe
cristianreyes214 <cristian.reyes@uao.edu.co>
2019-11-06 15:37:27 -0500

    Se adelantó el Header2

--- src/Components/Header/Header2.js
+++ src/Components/Header/Header2.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 import './Header2.css'
 
-const Header2 = () => {
+const Header2 = (props) => {
     return (
         <div className="o-header2">
-            <div className="o-access-container">
-                <h2>E-come</h2>
-                <h4>by Hypnotic</h4>
+            <div className="o-credits-container">
+                <h2 className="o-title-header2">E-come</h2>
+                <h4 className="o-title-header2">by Hypnotic</h4>
             </div>
-            <img alt=""  className="o-img-profile" />
+            <img alt="" src={props.img} className="o-img-profile" />
         </div>
     )
 }
