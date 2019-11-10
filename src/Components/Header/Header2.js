a4d1b419134095f6ebf45ae115b911b78d546c78
juanda3098 <juandapore3098@gmail.com>
2019-11-10 14:04:32 -0500

    Se modificó el estilo del componente

--- src/Components/Header/Header2.js
+++ src/Components/Header/Header2.js
@@ -6,8 +6,8 @@ const Header2 = (props) => {
     return (
         <div className="o-header2">
             <div className="o-credits-container">
-                <h2 className="o-title-header2">E-come</h2>
-                <h4 className="o-title-header2">by Hypnotic</h4>
+                <h3 className="o-title-header2">E-come</h3>
+                <h5 className="o-title-header2">by Hypnotic</h5>
             </div>
             <img alt="" src={props.img} className="o-img-profile" />
         </div>
