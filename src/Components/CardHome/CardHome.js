9f68ff840b0ae2e49ce74382719ffb3740c557ef
juanda3098 <juandapore3098@gmail.com>
2019-11-10 14:03:35 -0500

    Se agregó el componente CardHome

---
+++ src/Components/CardHome/CardHome.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+import './CardHome.css';
+
+class CardHome extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return(
+            <div>cardhome</div>
+        )
+    }
+
+}
+
+export default CardHome
