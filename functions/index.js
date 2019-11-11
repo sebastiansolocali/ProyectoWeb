d1c998b199e7596e1fe8b0f66f9db7292e956793
juanda3098 <juandapore3098@gmail.com>
2019-11-11 01:13:58 -0500

    Se implementó un Endpoint de prueba

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,21 @@
-const functions = require('firebase-functions');
-
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+//
+// URL: https://us-central1-proyectoweb-67fae.cloudfunctions.net/app
+
+const functions = require("firebase-functions");
+const express = require("express");
+const admin = require("firebase-admin");
+
+const app = express();
+admin.initializeApp();
+
+app.get('/functions', (req, res) => {
+    res.send("express desde firebase cloud functions")
+});
+
+exports.app = functions.https.onRequest(app);
