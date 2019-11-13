fd87bcf1858a96210381c152d563872e05d1da2b
Lab Informatica <lab.informatica@uao.edu.co>
2019-11-12 20:44:52 -0500

    Se comenzó la conección con database desde la api

--- functions/index.js
+++ functions/index.js
@@ -9,13 +9,32 @@
 
 const functions = require("firebase-functions");
 const express = require("express");
-const admin = require("firebase-admin");
+const admin = require("firebase");
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBBuGV1dEsO79Fz4rLZgdHicSPPcwgxjl8",
+    authDomain: "proyectoweb-67fae.firebaseapp.com",
+    databaseURL: "https://proyectoweb-67fae.firebaseio.com",
+    projectId: "proyectoweb-67fae",
+    storageBucket: "proyectoweb-67fae.appspot.com",
+    messagingSenderId: "143404945456",
+    appId: "1:143404945456:web:4f7ce2494eb3a6d36b9df5",
+    measurementId: "G-X12CFKXDG5"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 const app = express();
 admin.initializeApp();
 
 app.get('/functions', (req, res) => {
-    res.send("hola con express desde firebase cloud functions")
+    var consulta;
+    const db = firebase.database();
+    db.ref("/usuarios").on("value", data => {
+        consulta = data.toJSON();
+    });
+
+    res.send(consulta)
 });
 
 exports.app = functions.https.onRequest(app);
