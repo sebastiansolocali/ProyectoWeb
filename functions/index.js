21bec8315e3f3c77ecd31f105fc871f67e6cabae
juanda3098 <juandapore3098@gmail.com>
2019-11-13 22:39:09 -0500

    Se inicio el CRUD en la api

--- functions/index.js
+++ functions/index.js
@@ -9,32 +9,72 @@
 
 const functions = require("firebase-functions");
 const express = require("express");
-const admin = require("firebase");
+const firebase = require("firebase");
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBBuGV1dEsO79Fz4rLZgdHicSPPcwgxjl8",
-    authDomain: "proyectoweb-67fae.firebaseapp.com",
-    databaseURL: "https://proyectoweb-67fae.firebaseio.com",
-    projectId: "proyectoweb-67fae",
-    storageBucket: "proyectoweb-67fae.appspot.com",
-    messagingSenderId: "143404945456",
-    appId: "1:143404945456:web:4f7ce2494eb3a6d36b9df5",
-    measurementId: "G-X12CFKXDG5"
+  apiKey: "AIzaSyBBuGV1dEsO79Fz4rLZgdHicSPPcwgxjl8",
+  authDomain: "proyectoweb-67fae.firebaseapp.com",
+  databaseURL: "https://proyectoweb-67fae.firebaseio.com",
+  projectId: "proyectoweb-67fae",
+  storageBucket: "proyectoweb-67fae.appspot.com",
+  messagingSenderId: "143404945456",
+  appId: "1:143404945456:web:4f7ce2494eb3a6d36b9df5",
+  measurementId: "G-X12CFKXDG5"
 };
 
 firebase.initializeApp(firebaseConfig);
 
 const app = express();
-admin.initializeApp();
+app.use(express.json());
 
-app.get('/functions', (req, res) => {
-    var consulta;
-    const db = firebase.database();
-    db.ref("/usuarios").on("value", data => {
-        consulta = data.toJSON();
+// ------------------ GET
+app.get("/functions/:user", (req, res) => {
+  var consulta;
+
+  firebase
+    .database()
+    .ref(`/usuarios/${req.params.user}`)
+    .on("value", data => {
+      consulta = data;
+    });
+  console.log(`Se cosultó sobre ${req.params.user}`);
+  res.send(consulta);
+});
+
+// ------------------ POST
+app.post("/functions", (req, res) => {
+  firebase
+    .database()
+    .ref(`/usuarios/${req.body.username}`)
+    .set({
+      name: req.body.name,
+      password: req.body.password
     });
 
-    res.send(consulta)
+  console.log(`Se inserto ${req.body.username}`);
+  res.send(`Se insertó ${req.body.username}`);
+});
+
+// ------------------ UPDATE
+app.put("/functions", (req, res) => {
+  var updates = {};
+
+  if (req.body.newPassword !== undefined) {
+    console.log(`Contraseña: ${req.body.newPassword}`);
+    updates[`/usuarios/${req.body.username}/password`] = req.body.newPassword;
+  }
+  if (req.body.newName !== undefined) {
+    console.log(`Name: ${req.body.newName}`);
+    updates[`/usuarios/${req.body.username}/name`] = req.body.newName;
+  }
+
+  firebase
+    .database()
+    .ref()
+    .update(updates);
+
+  console.log(`Se modificó ${req.body.username}`);
+  res.send(`Se modificó ${req.body.username}`);
 });
 
 exports.app = functions.https.onRequest(app);
