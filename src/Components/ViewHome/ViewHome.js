ce0c709fa1a06a0c2c3f66e5be4c4ad42d5e5e33
juanda3098 <juandapore3098@gmail.com>
2019-11-17 11:05:46 -0500

    Se creó el componente ViewHome

---
+++ src/Components/ViewHome/ViewHome.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+
+import './ViewHome.css'
+
+class ViewHome extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return(
+            <div>ViewHome</div>
+        )
+    }
+
+}
+
+export default ViewHome
