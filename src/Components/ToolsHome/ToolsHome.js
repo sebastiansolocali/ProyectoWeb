c21414666d28e033cc207afb0b3b48f13320a111
juanda3098 <juandapore3098@gmail.com>
2019-11-17 11:05:27 -0500

    Se creó el componente ToolsHome

---
+++ src/Components/ToolsHome/ToolsHome.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+
+import './ToolsHome.css'
+
+class ToolsHome extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return(
+            <div>toolsHome</div>
+        )
+    }
+
+}
+
+export default ToolsHome
