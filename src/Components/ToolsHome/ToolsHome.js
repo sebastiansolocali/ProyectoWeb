0a7d64eef44cf6eb0316e4e6f897c1df390bada5
juanda3098 <juandapore3098@gmail.com>
2019-11-17 19:29:19 -0500

    Se ajustaron algunas propiedades de componentes

--- src/Components/ToolsHome/ToolsHome.js
+++ src/Components/ToolsHome/ToolsHome.js
@@ -7,15 +7,27 @@ class ToolsHome extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      width: 200,
-      height: 200,
-      x: 10,
-      y: 10
+      rooms: {
+        room1: {
+          width: 200,
+          height: 200,
+          x: 10,
+          y: 10
+        }
+      },
+      sensors: {
+        sensor1: {
+          width: 50,
+          height: 50,
+          x: 10,
+          y: 10
+        }
+      }
     };
   }
 
-  UNSAFE_componentWillUpdate(prevProps, prevState, snapshot){
-      console.log(prevState)
+  UNSAFE_componentWillUpdate(prevProps, prevState, snapshot) {
+    console.log(prevState);
   }
 
   render() {
@@ -26,23 +38,48 @@ class ToolsHome extends Component {
           <button className="o-button">Back</button>
         </div>
         <div className="o-design">
-            {console.log(this.state)}
           <Rnd
             className="o-room"
-            size={{ width: this.state.width, height: this.state.height }}
-            position={{ x: this.state.x, y: this.state.y }}
+            size={{
+              width: this.state.rooms.room1.width,
+              height: this.state.rooms.room1.height
+            }}
+            position={{
+              x: this.state.rooms.room1.x,
+              y: this.state.rooms.room1.y
+            }}
             onDragStop={(e, d) => {
-              this.setState({ x: d.x, y: d.y });
+              this.setState({ rooms: { room1: { x: d.x, y: d.y } } });
             }}
             onResizeStop={(e, direction, ref, delta, position) => {
               this.setState({
-                width: ref.style.width,
-                height: ref.style.height,
-                ...position
+                rooms: {
+                  room1: {
+                    width: ref.style.width,
+                    height: ref.style.height,
+                    ...position
+                  }
+                }
               });
             }}
           >
             <p>Cuarto1</p>
+          </Rnd>
+          <Rnd
+            className="o-sensor"
+            size={{
+              width: this.state.sensors.sensor1.width,
+              height: this.state.sensors.sensor1.height
+            }}
+            position={{
+              x: this.state.sensors.sensor1.x,
+              y: this.state.sensors.sensor1.y
+            }}
+            onDragStop={(e, d) => {
+              this.setState({ sensors: { sensor1: { x: d.x, y: d.y } } });
+            }}
+          >
+            <p>sensor1</p>
           </Rnd>
         </div>
         <div className="o-details">
