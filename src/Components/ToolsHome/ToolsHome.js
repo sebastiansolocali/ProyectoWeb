f74b133b184f03d260ac9aa78fa2baee6d70d613
juanda3098 <juandapore3098@gmail.com>
2019-11-18 11:18:19 -0500

    Se creó la casa con sus componentes

--- src/Components/ToolsHome/ToolsHome.js
+++ src/Components/ToolsHome/ToolsHome.js
@@ -8,26 +8,41 @@ class ToolsHome extends Component {
     super(props);
     this.state = {
       rooms: {
-        room1: {
-          width: 200,
-          height: 200,
-          x: 10,
-          y: 10
-        }
+        // room: {
+        //   id: 1,
+        //   width: 200,
+        //   height: 200,
+        //   x: 10,
+        //   y: 10
+        // }
       },
       sensors: {
-        sensor1: {
-          width: 50,
-          height: 50,
-          x: 10,
-          y: 10
-        }
+        // sensor: {
+        //   id: 1,
+        //   width: 40,
+        //   height: 40,
+        //   x: 10,
+        //   y: 10
+        // }
       }
     };
   }
 
+  UNSAFE_componentWillMount() {
+
+    // console.log(prevState);
+  }
+
   UNSAFE_componentWillUpdate(prevProps, prevState, snapshot) {
-    console.log(prevState);
+    // console.log(prevState);
+  }
+
+  addRoom() {
+
+  }
+
+  addSensor() {
+
   }
 
   render() {
@@ -38,53 +53,87 @@ class ToolsHome extends Component {
           <button className="o-button">Back</button>
         </div>
         <div className="o-design">
-          <Rnd
-            className="o-room"
-            size={{
-              width: this.state.rooms.room1.width,
-              height: this.state.rooms.room1.height
-            }}
-            position={{
-              x: this.state.rooms.room1.x,
-              y: this.state.rooms.room1.y
-            }}
-            onDragStop={(e, d) => {
-              this.setState({ rooms: { room1: { x: d.x, y: d.y } } });
-            }}
-            onResizeStop={(e, direction, ref, delta, position) => {
-              this.setState({
-                rooms: {
-                  room1: {
-                    width: ref.style.width,
-                    height: ref.style.height,
-                    ...position
-                  }
-                }
-              });
-            }}
-          >
-            <p>Cuarto1</p>
-          </Rnd>
-          <Rnd
-            className="o-sensor"
-            size={{
-              width: this.state.sensors.sensor1.width,
-              height: this.state.sensors.sensor1.height
-            }}
-            position={{
-              x: this.state.sensors.sensor1.x,
-              y: this.state.sensors.sensor1.y
-            }}
-            onDragStop={(e, d) => {
-              this.setState({ sensors: { sensor1: { x: d.x, y: d.y } } });
-            }}
-          >
-            <p>sensor1</p>
-          </Rnd>
+          {Object.values(this.state.rooms).map((room, index) => {
+            return (
+              <div key={index}>
+                <Rnd
+                  className="o-room"
+                  size={{
+                    width: room.width,
+                    height: room.height
+                  }}
+                  position={{
+                    x: room.x,
+                    y: room.y
+                  }}
+                  onDragStop={(e, d) => {
+                    this.setState({
+                      rooms: {
+                        index: {
+                          id: index + 1,
+                          width: room.width,
+                          height: room.height,
+                          x: d.x,
+                          y: d.y
+                        }
+                      }
+                    });
+                  }}
+                  onResizeStop={(e, direction, ref, delta, position) => {
+                    this.setState({
+                      rooms: {
+                        index: {
+                          id: index + 1,
+                          width: ref.style.width,
+                          height: ref.style.height,
+                          ...position
+                        }
+                      }
+                    });
+                  }}
+                >
+                  <p>Cuarto</p>
+                </Rnd>
+              </div>
+            );
+          })}
+          
+          {Object.values(this.state.sensors).map((sensor, index) => {
+            return (
+              <div key={index}>
+                <Rnd
+                  className="o-sensor"
+                  size={{
+                    width: sensor.width,
+                    height: sensor.height
+                  }}
+                  position={{
+                    x: sensor.x,
+                    y: sensor.y
+                  }}
+                  onDragStop={(e, d) => {
+                    this.setState({
+                      sensors: {
+                        index: {
+                          id: index + 1,
+                          width: sensor.width,
+                          height: sensor.height,
+                          x: d.x,
+                          y: d.y
+                        }
+                      }
+                    });
+                  }}
+                >
+                  <p>sensor1</p>
+                </Rnd>
+              </div>
+            );
+          })}
         </div>
         <div className="o-details">
-          <div>design</div>
-          <div>background</div>
+          <button onClick={this.addRoom()} className="o-button">Add Room</button>
+          <button onClick={this.addSensor()} className="o-button">Add Sensor</button>
         </div>
       </div>
     );
